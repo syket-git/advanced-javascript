78734e2a730eeb708595aa2c5c3dec7e20f7cc49
syket-git <syketb@gmail.com>
2020-03-03 23:43:08 +0600

    Truth & Falsy Practicing

---
+++ true-false.js
@@ -0,0 +1,9 @@
+// Javascript Falsy : 0, null, undefined, "", NaN, false
+// Javascript Truthy: anyNumber, anyString, " ", "0", [], {}, true
+
+const name = [];
+if(name){
+    console.log("console is true");
+}else{
+    console.log("console is false");
+}
