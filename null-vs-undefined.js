ec34fcafe77a2aaf0d6e55f11b642723a2bdb8ee
syket-git <syketb@gmail.com>
2020-03-04 00:08:24 +0600

    Null-vs-Undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+// let name;
+// console.log(name);
+
+// function doSomething(x, y){
+//     console.log(y);
+// }
+// doSomething(32);
+
+// function doSomething(num1, num2){
+//     console.log(num1 + num2);
+//  }
+// const result = doSomething(2, 4);
+// console.log(result); 
+
+// const obj = {name: "syket", id: 941458}
+// console.log(obj.dep);
+
+// let name = undefined;
+// console.log(name);
+
+
+var age = [3, 6, 3];
+console.log(age[10]);
